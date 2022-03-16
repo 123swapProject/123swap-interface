f8bd6af2fee192b7414db7d24e9337aac63dce43
onetwothreeswap <onetwothreeswap@gmail.com>
2022-03-16 17:21:52 +0200

    Removed "add sushi", analytics link

--- src/modals/NetworkModal/index.tsx
+++ src/modals/NetworkModal/index.tsx
@@ -206,8 +206,7 @@ export default function NetworkModal(): JSX.Element | null {
     <Modal isOpen={networkModalOpen} onDismiss={toggleNetworkModal} maxWidth={672}>
       <ModalHeader onClose={toggleNetworkModal} title="Select a Network" />
       <div className="mb-6 text-lg text-primary">
-        You are currently browsing <span className="font-bold text-pink">SUSHI</span>
-        <br /> on the <span className="font-bold text-blue">{NETWORK_LABEL[chainId]}</span> network
+        You are currently on the <span className="font-bold text-blue">{NETWORK_LABEL[chainId]}</span> network
       </div>
 
       <div className="grid grid-flow-row-dense grid-cols-1 gap-5 overflow-y-auto md:grid-cols-2">
